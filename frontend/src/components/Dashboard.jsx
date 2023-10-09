2d7fae5344d48cd84b230f5abf2f396a558203ad
alifsyr <alifsyr@gmail.com>
2023-10-09 10:31:54 +0700

    update

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect }from 'react'
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+import { useNavigate } from 'react-router-dom';
 import '../styles/Dashboard.css'
 import SearchBar from './SearchBar';
+import JobList from './JobList';
 
 function Dashboard() {
     return (
         <div className="dashboard">
-            <div className="header ">
+            <div className="header">
                 <h1><b>GitHub</b> Jobs</h1>
+                <div>
+                    <button className='button is-light'>Log Out</button>
+                </div>
             </div>
             <SearchBar />
+            <JobList />
         </div>
     )
 }
 
-export default Dashboard
+export default Dashboard;
--- frontend/src/components/JobList.jsx
+++ frontend/src/components/JobList.jsx
@@ -1,10 +1,68 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+import { useNavigate } from 'react-router-dom';
 import '../styles/JobList.css'
 
 function JobList() {
-    return (
-    
+  const [page, setPage] = useState(1);
+  const [token, setToken] = useState('');
+  const [expired, setExpired] = useState('');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    refreshToken();
+    getJobList();
+  }, []);
+
+  const axiosJWT = axios.create();
+
+  axiosJWT.interceptors.response.use(async (config) => {
+    const currentDate = new Date();
+    if (expired * 1000 < currentDate.getTime()) {
+      const res = await axios.get('http://localhost:3000/token');
+      config.headers.Authorization = `Bearer ${res.data.accessToken}`;
+      setToken(res.data.accessToken);
+      const decoded = jwt_decode(res.data.accessToken);
+      setExpired(decoded.exp);
+    }
+    return config;
+  }, (error) => {
+    return Promise.reject(error);
+  });
+
+  const refreshToken = async () => {
+    try {
+      const res = await axios.get('http://localhost:3000/token');
+      setToken(res.data.accessToken);
+      const decoded = jwt_decode(res.data.accessToken);
+      setExpired(decoded.exp);
+
+    } catch (error) {
+      console.log(error);
+      if (error.response) {
+        navigate.push('/')
+      }
+    }
+  }
+
+  const getJobList = async () => {
+    try {
+      const res = await axiosJWT.get('http://localhost:3000/jobs', {
+        headers: { Authorization: `Bearer ${token}` },
+        params: { page }
+      });
+      console.log(res.data);
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  return (
+    <div className="container">
+      <p>Hello</p>
+    </div>
   )
 }
 
-export default JobList
+export default JobList;
